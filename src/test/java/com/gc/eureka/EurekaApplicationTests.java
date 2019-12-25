507f417ba8aab9090610bad7bd146e128d31828c
unknown <405200875@qq.com>
2019-12-25 22:14:22 +0800

    init

---
+++ src/test/java/com/gc/eureka/EurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gc.eureka;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+public class EurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
